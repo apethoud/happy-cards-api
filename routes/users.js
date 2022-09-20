34d2360cbf4e7d788f4a1de955161a3be81b46d3
Andrew Pethoud <andrewpethoud@gmail.com>
2022-09-19 20:34:48 -0700

    hash user passwords before saving

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const Joi = require("joi");
 const _ = require("lodash");
+const bcrypt = require("bcrypt");
 const User = require("../models/User");
 
 async function createUser(usr) {
@@ -22,7 +23,9 @@ router.post("/", async (req, res) => {
   let user = await User.findOne(_.pick(req.body, ["email"]));
   if (user) res.status(400).send("User already registered.");
 
-  user = _.pick(req.body, ["name", "email", "password"]);
+  user = new User(_.pick(req.body, ["name", "email", "password"]));
+  const salt = await bcrypt.genSalt(10);
+  user.password = bcrypt.hash(user.password, salt);
 
   createUser(user)
     .then((response) => res.send(_.pick(response, ["_id", "name", "email"])))
